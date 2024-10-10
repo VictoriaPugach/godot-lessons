8801e822abcb5e3560c463c9c92fef23b72436f9
VictoriaPugach <pugachviktoria@yandex.ru>
2024-10-11 01:01:15 +0300

    Added jump sound

--- lesson_1_collectables/coin.gd
+++ lesson_1_collectables/coin.gd
@@ -1,7 +1,9 @@
 extends Area2D
+#@onready var jump_sound = $"../AudioStreamPlayer2D"
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
+		#jump_sound.play()
 		body.coins += 1
 		self.queue_free()
 		print("Количество собранных монет =", body.coins)
