7f429b8f8f1d404d67d5b387721c4f5740586f4d
VictoriaPugach <pugachviktoria@yandex.ru>
2024-10-11 14:33:21 +0300

    Added an enemy...

--- lesson_2_basic_mechanics/gui.gd
+++ lesson_2_basic_mechanics/gui.gd
@@ -3,9 +3,9 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$hp_bar.max_value = get_node("../Player").health
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$hp_bar.value = get_node("../Player").health
