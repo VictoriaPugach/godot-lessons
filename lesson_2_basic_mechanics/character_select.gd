448b0947d4db07cf37fd6c17ee3f349391055f7e
VictoriaPugach <pugachviktoria@yandex.ru>
2024-10-12 01:26:28 +0300

    Implemented the functionality for watching through all the characters

---
+++ lesson_2_basic_mechanics/character_select.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+func _process(delta: float) -> void:
+	match Game.PlayerSelect:
+		0:
+			get_node("PlayerSelect").play("Player0")
+		1:
+			get_node("PlayerSelect").play("Player1")
+		2:
+			get_node("PlayerSelect").play("Player2")
+		3:
+			get_node("PlayerSelect").play("Player3")
+
+
+func _on_left_pressed() -> void:
+	if Game.PlayerSelect > 0:
+		Game.PlayerSelect -= 1
+
+
+func _on_right_pressed() -> void:
+	if Game.PlayerSelect < 4: # так как у меня 4 персонажа
+		Game.PlayerSelect += 1
